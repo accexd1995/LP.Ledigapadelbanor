b3871341473e68ac072b1f00b8c90fc05a23fc48
axeel95 <axeel95@gmail.com>
2020-06-24 19:04:05 +0200

    Added padelcourt database

---
+++ LP.API/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LP.API
+{
+    public class MyContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<PadelCourt> PadelCourts { get; set; }
+    }
+}
--- LP.API/Startup.cs
+++ LP.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,8 @@ namespace LP.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            // Add framework services.
+            services.AddDbContext<MyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
